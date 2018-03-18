79e48c97986cf13237276b23b55dfb21922b9586
Ha Quoc Tien <17152112@gm.uit.edu.vn>
2018-03-18 21:12:29 +0700

    Implement reverseString, enable test cases

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-var reverseString = function() {
-
+var reverseString = function(str) {
+  var result = '';
+  let l = str.length;
+  var i;
+  for(i = l; i >= 0; i--) {
+    result += str.charAt(i);
+  }
+  return result;
 }
 
 module.exports = reverseString
