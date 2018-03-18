2443c237bd79f2880f59f5be6286294ae7c410b0
Ha Quoc Tien <17152112@gm.uit.edu.vn>
2018-03-18 21:07:21 +0700

    Implement repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-var repeatString = function() {
-
+var repeatString = function(str, times) {
+  var result = '';
+  var i;
+  if (times < 0) {
+    return "ERROR";
+  }
+  for (i = 0; i < times; i++) {
+    result += str;
+  }
+  return result;
 }
 
 module.exports = repeatString
